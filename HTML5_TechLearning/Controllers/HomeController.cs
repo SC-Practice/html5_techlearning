64d159b28c2c58ec0a8ef909c78105704f63f36c
daniuchens <daniuchens@gmail.com>
2015-12-21 18:13:21 +0800

    讓TeamMember資料來自Controller產生

--- HTML5_TechLearning/Controllers/HomeController.cs
+++ HTML5_TechLearning/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HTML5_TechLearning.Models.TeamMember;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -34,7 +35,133 @@ namespace HTML5_TechLearning.Controllers
 
         public ActionResult TeamMember()
         {
-            return View();
+            TeamMemberViewModel model = new TeamMemberViewModel() {
+                PageTitle = "TW B2B TEAM",
+                Groups = new List<GroupInfo>()
+            };
+
+            var group1 = new GroupInfo() {
+                Title = "3W",
+                Members = new List<MemberInfo>()
+            };
+
+            group1.Members.Add(new MemberInfo() {
+                ChineseName = "陳瑩聰",
+                ImageFileName = "Daniel.jpg",
+                NickName = "Daniel"
+            });
+
+            group1.Members.Add(new MemberInfo()
+            {
+                ChineseName = "陳瑩聰",
+                ImageFileName = "Daniel.jpg",
+                NickName = "Daniel"
+            });
+
+            group1.Members.Add(new MemberInfo()
+            {
+                ChineseName = "陳瑩聰",
+                ImageFileName = "Daniel.jpg",
+                NickName = "Daniel"
+            });
+
+            group1.Members.Add(new MemberInfo()
+            {
+                ChineseName = "陳瑩聰",
+                ImageFileName = "Daniel.jpg",
+                NickName = "Daniel"
+            });
+
+            group1.Members.Add(new MemberInfo()
+            {
+                ChineseName = "陳瑩聰",
+                ImageFileName = "Daniel.jpg",
+                NickName = "Daniel"
+            });
+
+            group1.Members.Add(new MemberInfo()
+            {
+                ChineseName = "陳瑩聰",
+                ImageFileName = "Daniel.jpg",
+                NickName = "Daniel"
+            });
+
+            group1.Members.Add(new MemberInfo()
+            {
+                ChineseName = "陳瑩聰",
+                ImageFileName = "Daniel.jpg",
+                NickName = "Daniel"
+            });
+            
+            group1.Members.Add(new MemberInfo()
+            {
+                ChineseName = "童俊傑",
+                ImageFileName = "comingsoon.png",
+                NickName = "Jerry"
+            });
+
+            model.Groups.Add(group1);
+
+            var group2 = new GroupInfo()
+            {
+                Title = "SSL",
+                Members = new List<MemberInfo>()
+            };
+
+            group2.Members.Add(new MemberInfo()
+            {
+                ChineseName = "陳智興",
+                ImageFileName = "comingsoon.png",
+                NickName = "Sean"
+            });
+
+            group2.Members.Add(new MemberInfo()
+            {
+                ChineseName = "林承翰",
+                ImageFileName = "comingsoon.png",
+                NickName = "Sin"
+            });
+
+            model.Groups.Add(group2);
+
+            var group3 = new GroupInfo()
+            {
+                Title = "Testing",
+                Members = new List<MemberInfo>()
+            };
+
+            group3.Members.Add(new MemberInfo()
+            {
+                ChineseName = "林炘慧",
+                ImageFileName = "comingsoon.png",
+                NickName = "Orange"
+            });
+
+            group3.Members.Add(new MemberInfo()
+            {
+                ChineseName = "沈介民",
+                ImageFileName = "comingsoon.png",
+                NickName = "Jonathan"
+            });
+
+            model.Groups.Add(group3);
+
+            var group4 = new GroupInfo()
+            {
+                Title = "PM",
+                Members = new List<MemberInfo>()
+            };
+
+            group4.Members.Add(new MemberInfo()
+            {
+                ChineseName = "陳旭超",
+                ImageFileName = "comingsoon.png",
+                NickName = "Super"
+            });
+
+            model.Groups.Add(group4);
+
+            return View(model);
         }
     }
 }
---
+++ HTML5_TechLearning/Models/TeamMember/TeamMemberViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HTML5_TechLearning.Models.TeamMember
+{
+    public class TeamMemberViewModel
+    {
+        /// <summary>
+        /// 頁面大標題.
+        /// </summary>
+        public string PageTitle { get; set; }
+
+        /// <summary>
+        /// 部門下的子部門.
+        /// </summary>
+        public List<GroupInfo> Groups { get; set; }
+    }
+}
