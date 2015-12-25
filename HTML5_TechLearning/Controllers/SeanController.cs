7430e3968d273181b17868086862b73bd8fba141
SeanChen <livebreeze@gmail.com>
2015-12-25 10:46:29 +0800

    move sean page into controller

---
+++ HTML5_TechLearning/Controllers/SeanController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTML5_TechLearning.Controllers
+{
+    public class SeanController : Controller
+    {
+        // GET: Sean
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BetterBoxes()
+        {
+            return View();
+        }
+
+        public ActionResult Transition()
+        {
+            return View();
+        }
+    }
+}
